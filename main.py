8128e19afcadb694b5ee32eac0a4f2e45b8ccf0b
MozhaevVN <mozhaev.vov@ya.ru>
2024-09-07 17:35:02 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 homework_completed = 12
 hours_spent = 1.5
 course_name = 'Python'
-time_spent_on_1_homework = '0.125 часа'
+time_spent_on_1_homework = hours_spent / homework_completed
 print('Курс:', course_name, ', всего задач:', homework_completed, ', затрачено часов:', hours_spent, ', среднее время выполнения:', time_spent_on_1_homework)
 
